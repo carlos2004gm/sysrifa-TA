02507d09bcbe582d257ac431a2cb13e9cd467e20
carlos2004gm <carlos.marques.cgm@gmail.com>
2022-06-03 13:37:22 -0300

    commit

--- src/conexao/conn.php
+++ src/conexao/conn.php
@@ -1,18 +1,17 @@
 <?php
 
-   // Carregar as credencias do banco de dados
- $hostname = "sql212.epizy.com";
- $database = "epiz_31521520_sysrifa";
- $user = "epiz_31521520";
- $password = "JYaad6JZ9w";
+  // Carregar as credencias do banco de dados
+  $hostname = "sql212.epizy.com";
+  $database = "epiz_31521520_sysrifa";
+  $user = "epiz_31521520";
+  $password = "JYaad6JZ9w";
 
-   try{
-       $pdo = new PDO('mysql:host='.$hostname.';dbname='.$database, $user,  $password);
-       $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-   } catch () {
+  try{
+    $pdo = new PDO('mysql:host='.$hostname.';dbname='.$database, $user,  $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo 'Conexão com o banco de dados ' . $database . 'foi realizado com sucesso!';
+  }catch(PDOException $e){
+    echo 'Erro: ' . $e->getMessage();
+  }
 
-   }
-
-   
-   
-   
+?>      
